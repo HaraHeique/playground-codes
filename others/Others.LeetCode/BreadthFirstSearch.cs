8a5d5b6a4f2a428888745bf72fc6588a60c5811f
HaraHeique <heikacademicos@hotmail.com>
2025-02-16 20:11:35 -0300

    feat: implementations of BFS

--- others/Others.LeetCode/BreadthFirstSearch.cs
+++ others/Others.LeetCode/BreadthFirstSearch.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+
 namespace Others.LeetCode;
 
 public class BreadthFirstSearchSolution
@@ -42,6 +44,9 @@ public class BreadthFirstSearchSolution
 
             return this;
         }
+
+        public bool HasLeftChild() => LeftChild is not null;
+        public bool HasRightChild() => RightChild is not null;
     }
 
     public static void Execute(params string[] _) 
@@ -53,7 +58,11 @@ public class BreadthFirstSearchSolution
                 TreeNode.Construct(2)
                     .WithLeftChild(
                         TreeNode.Construct(4)
-                            .WithLeftChild(TreeNode.Construct(7))
+                            .WithLeftChild(
+                                TreeNode.Construct(7)
+                                    .WithLeftChild(TreeNode.Construct(9))
+                                    .WithRightChild(TreeNode.Construct(10))
+                            )
                             .WithRightChild(TreeNode.Construct(8))
                     )
             )
@@ -63,6 +72,93 @@ public class BreadthFirstSearchSolution
                     .WithRightChild(TreeNode.Construct(6))
             );
 
+        MilanSolution(rootNode);
+        Console.WriteLine();
+
+        Console.WriteLine();
+        RecursionSolution(rootNode);
+        Console.WriteLine();
+
         Console.WriteLine("BFS Solution has been finished!");
+    }
+
+    /// <summary>
+    /// Solução delete mostrada aqui: https://www.youtube.com/watch?v=WIVkVToFmrY
+    /// </summary>
+    /// <param name="root"></param>
+    private static void MilanSolution(TreeNode root) 
+    {
+        Queue<(TreeNode Node, int Level)> queue = new();
+
+        queue.Enqueue((root, 1));
+        int currentLevel = queue.Peek().Level;
+
+        while (queue.Count > 0)
+        {
+            (TreeNode currentNode, int level) = queue.Dequeue();
+
+            if (currentLevel < level) 
+            {
+                Console.WriteLine();
+                currentLevel = level;
+            }
+
+            Console.Write($"{currentNode.Value} ");
+
+            if (currentNode.HasLeftChild())
+            {
+                queue.Enqueue((currentNode.LeftChild!, level + 1));
+            }
+
+            if (currentNode.HasRightChild())
+            {
+                queue.Enqueue((currentNode.RightChild!, level + 1));
+            }
+        }
+    }
+
+    /// <summary>
+    /// Minha solução usando recursão, que não é melhor que a do Milan
+    /// </summary>
+    /// <param name="root"></param>
+    private static void RecursionSolution(TreeNode root) 
+    {
+        PriorityQueue<(int Value, int Level), string> values = new();
+
+        values.Enqueue((root.Value, 1), "1");
+        int numberOfSteps = 0;
+
+        Execute(root, currentLevel: 1);
+        ShowResult();
+
+        void Execute(TreeNode? currentNode, int currentLevel) 
+        {
+            if (currentNode is null) return;
+
+            int nextLevel = currentLevel + 1;
+
+            if (currentNode.HasLeftChild()) 
+                values.Enqueue((currentNode.LeftChild!.Value, nextLevel), $"{nextLevel}.{numberOfSteps++}");
+
+            if (currentNode.HasRightChild()) 
+                values.Enqueue((currentNode.RightChild!.Value, nextLevel), $"{nextLevel}.{numberOfSteps++}");
+
+            Execute(currentNode.LeftChild, currentLevel + 1);
+            Execute(currentNode.RightChild, currentLevel + 1);
+        }
+
+        void ShowResult() 
+        {
+            int currentLevel = 1;
+
+            while (values.TryDequeue(out var item, out var _))
+            {
+                if (currentLevel != item.Level) Console.WriteLine();
+
+                Console.Write($"{item.Value} ");
+
+                currentLevel = item.Level;
+            }
+        }
     }
 }
